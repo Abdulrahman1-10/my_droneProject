a276a28dc33b3487d6aaad490faa0822de7eeb4d
Abdulrahman1-10 <158218291+Abdulrahman1-10@users.noreply.github.com>
2024-09-01 21:35:35 +0300

    Update ArmDrone.py

--- ArmDrone.py
+++ ArmDrone.py
@@ -1,4 +1,3 @@
-import argparse
 from dronekit import connect, VehicleMode
 import time
 
@@ -7,17 +6,14 @@ def arm_and_takeoff(vehicle):
     Arms the vehicle and takes off to a specified altitude.
     """
     print("Basic pre-arm checks")
-    # Don't let the user try to arm until autopilot is ready
     while not vehicle.is_armable:
         print(" Waiting for vehicle to initialise...")
         time.sleep(1)
 
     print("Arming motors")
-    # Set the vehicle to GUIDED mode and then arm
     vehicle.mode = VehicleMode("GUIDED")
     vehicle.armed = True
 
-    # Confirm vehicle armed before attempting to take off
     while not vehicle.armed:
         print(" Waiting for arming...")
         time.sleep(1)
@@ -25,20 +21,12 @@ def arm_and_takeoff(vehicle):
     print("Vehicle is armed!")
 
 def main():
-    # Set up command-line argument parsing
-    parser = argparse.ArgumentParser(description="Connect to a vehicle and arm it.")
-    parser.add_argument('--connect', type=str, required=True, help='Connection string to the vehicle')
-    args = parser.parse_args()
-
-    # Connect to the Vehicle (SITL or real vehicle)
-    connection_string = args.connect
+    # Connect to the vehicle using the ttyAMA0 serial port
+    connection_string = 'ttyAMA0'
     print(f"Connecting to vehicle on {connection_string}...")
     vehicle = connect(connection_string, wait_ready=True)
 
-    # Call the arm and takeoff function
     arm_and_takeoff(vehicle)
-
-    # Close vehicle object before exiting script
     vehicle.close()
 
     print("Script completed")
